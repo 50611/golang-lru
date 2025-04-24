78b1731de4b911480632cd36c98e25802d2280f8
sr <sr@qq.com>
2025-04-24 16:07:02 +0800

    v2 fix bug

--- lru.go
+++ lru.go
@@ -76,7 +76,7 @@ func (c *Cache[K, V]) GetOrAdd(key K, create func(k K) V) (bool, V) {
 		//no find add must false
 		val := create(key)
 		c.lru.Add(key, val)
-		return false, value
+		return false, val
 	}
 }
 
